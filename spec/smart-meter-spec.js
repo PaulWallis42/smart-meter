fab03fd445c574af34ffa5576fb6755e8bc20941
paulwallis42 <paulwallis42@gmail.com>
2017-04-25 10:22:30 +0100

    usage increase and reset tests passing

--- spec/smart-meter-spec.js
+++ spec/smart-meter-spec.js
@@ -10,6 +10,15 @@ describe('smart meter', function(){
     expect(meter.usage).toEqual(0);
   });
 
-  
+  it('can increase energy useage by 1', function(){
+    meter.increaseUsage();
+    expect(meter.usage).toEqual(1);
+  });
+
+  it('can reset the energy usage', function(){
+    meter.increaseUsage();
+    meter.reset();
+    expect(meter.usage).toEqual(0);
+  });
 
 });
--- src/smart-meter.js
+++ src/smart-meter.js
@@ -1,3 +1,11 @@
 function SmartMeter(){
   this.usage = 0;
 }
+
+SmartMeter.prototype.increaseUsage = function(){
+  this.usage ++;
+}
+
+SmartMeter.prototype.reset = function(){
+  this.usage = 0;
+}
