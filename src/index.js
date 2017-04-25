0e6b6862e521afc2195a2283c4f32c8f8a9bd783
paulwallis42 <paulwallis42@gmail.com>
2017-04-25 22:42:12 +0100

    second appliance added to index.html & index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ window.onload = function(){
 
   var meter = new SmartMeter();
   var oven = new Appliance(2);
+  var fridge = new Appliance(1);
+
   var usageDisplay = document.getElementById('usage');
   var ovenDiv = document.getElementById('oven');
+  var fridgeDiv = document.getElementById('fridge');
 
   setInterval(function(){
     usageDisplay.innerHTML = meter.usage;
@@ -14,6 +17,14 @@ window.onload = function(){
       oven.turnOff();
     } else {
       oven.turnOn(meter);
+    }
+  }
+
+  fridgeDiv.onclick = function(){
+    if(fridge.status){
+      fridge.turnOff();
+    } else {
+      fridge.turnOn(meter);
     }
   }
 
