a60f0d543171264eef6a4467bf7705ebc3e8eab0
paulwallis42 <paulwallis42@gmail.com>
2017-04-25 17:22:41 +0100

    appliance interval increase tested and passing

--- spec/appliance-spec.js
+++ spec/appliance-spec.js
@@ -10,14 +10,41 @@ describe('Appliance', function(){
     expect(oven.energyRating).toEqual(2);
   });
 
-  it('can be turned on', function(){
-    oven.turnOn();
-    expect(oven.status).toEqual(true);
+  it('has an initial energy usage of zero', function(){
+    expect(oven.energyUsage).toEqual(0);
   });
 
   it('can be turned off', function(){
     oven.turnOff();
     expect(oven.status).toEqual(false);
   });
+
+  it('increases energy usage continuously when turned on', function(){
+
+    jasmine.clock().install();
+
+    oven.turnOn();
+    jasmine.clock().tick(2001);
+
+    jasmine.clock().uninstall();
+
+    expect(oven.status).toEqual(true);
+    expect(oven.energyUsage).toEqual(4);
+  });
+
+  it('no longer increases energy usage when turned off', function(){
+
+    jasmine.clock().install();
+
+    oven.turnOn();
+    jasmine.clock().tick(2001);
+    oven.turnOff();
+    jasmine.clock().tick(1000);
+
+    jasmine.clock().uninstall();
+
+    expect(oven.energyUsage).toEqual(4);    
+  });
+
 
 });
--- src/appliance.js
+++ src/appliance.js
@@ -1,12 +1,19 @@
 function Appliance(energyRating){
   this.energyRating = energyRating;
   this.status = false;
+  this.energyUsage = 0;
+  this.myTimer;
 }
 
 Appliance.prototype.turnOn = function(){
+  var _this = this;
+  this.myTimer = setInterval(function(){
+    _this.energyUsage += _this.energyRating;
+  }, 1000);
   this.status = true;
 }
 
 Appliance.prototype.turnOff = function(){
+  clearInterval(this.myTimer);
   this.status = false;
 }
